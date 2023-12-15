42404359923aa52dba829cf7b9fdfcfc7c8b145c
user <user@DESKTOP-I4S0F6R>
2023-12-15 21:07:16 +0900

    Correct A.java and B.java

--- MyGit/src/mygit/B.java
+++ MyGit/src/mygit/B.java
@@ -4,6 +4,8 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("This is fist version of B.java");
+		
+		System.out.println("Correct B.java in devB branch.");
 	}
 
 }
